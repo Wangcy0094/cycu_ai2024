0f3945ebf600337bef62a1899e5861875708f1e4
王振宇 <152591242+Wangcy0094@users.noreply.github.com>
2024-04-02 12:12:26 +0000

    0812

--- 20240402/11272016_2.py
+++ 20240402/11272016_2.py
@@ -1,23 +1,30 @@
-#尋找台灣的區界地圖 shape file/ geojson file
-#https://data.gov.tw/dataset/7441
-
-#read 20240402/tract_20140313.json as geopanas
 import geopandas as gpd
 import pandas as pd
-
-#read shape file
-taiwan=gpd.read_file('20240402/county/COUNTY_MOI_1090820.shp')
-import os
-#print cwd
-print (taiwan)
-
-#plot taiwan using matplotlib
 import matplotlib.pyplot as plt
+from matplotlib.font_manager import FontProperties
 
-#輸出台灣地圖的時候 圖面顯示的範圍 緯度最低為21.5度 緯度最高為25.5度 經度最低為119度 經度最高為122度
+
+# 讀取 CSV 文件
+weather_data = pd.read_csv('20240402/weather_data.csv')
+
+# 讀取 shape file
+taiwan = gpd.read_file('20240402/county/COUNTY_MOI_1090820.shp')
+
+# 繪製台灣地圖
 taiwan.plot()
-plt.xlim(119,122)
-plt.ylim(21.5,25.5)
+plt.xlim(118,122)
+plt.ylim(21,27)
+
+
+# 在地圖上標出對應的溫度
+for i, row in weather_data.iterrows():
+    temp = row['Temperature']
+    # 獲取縣市的幾何中心點
+    city_geom = taiwan.loc[taiwan['COUNTYNAME'] == row['City']].geometry.unary_union.centroid
+    # 在繪製文字時使用設定的字體
+    plt.text(city_geom.x-0.35, city_geom.y, f'{temp}', fontsize=8)
+
 plt.show()
-#save to png file
-plt.savefig('20240402/taiwan_map.png')
+
+# 保存到 PNG 文件
+plt.savefig('20240402/taiwan_map_tempture.png')
