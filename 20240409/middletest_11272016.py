453c7374744eadb1ed2104edbc08da7a6b8a8644
王振宇 <152591242+Wangcy0094@users.noreply.github.com>
2024-04-09 11:23:36 +0000

    202404090723

--- 20240409/middletest_11272016.py
+++ 20240409/middletest_11272016.py
@@ -3,7 +3,11 @@ import requests
 # Replace with the actual URL of the CSV file
 csv_url = 'https://scweb.cwa.gov.tw/zh-tw/earthquake/csv/2024%E5%B9%B44%E6%9C%88'
 
-response = requests.get(csv_url)
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+}
+
+response = requests.get(csv_url, headers=headers)
 
 # Ensure the request was successful
 if response.status_code == 200:
