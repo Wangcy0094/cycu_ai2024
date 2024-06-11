aa86236fa10543cc9cff19dc72fad3e688a14cb4
王振宇 <152591242+Wangcy0094@users.noreply.github.com>
2024-06-11 11:30:20 +0000

    8888

--- 20240611/11272016.py
+++ 20240611/11272016.py
@@ -9,10 +9,9 @@ import requests
 #     data={"message": message}
 # )
 
-
-TOKEN_NOTIFY = 'iSWyrnS4AFuwUU9MTyx3z1rZ65QjSZeRW8Dbm3oZpK0'
-message = '測試'
-image_path = 'C:\\Users\\User\\Desktop\\AI\\image.png'
+TOKEN_NOTIFY = 'PDcpmD5Go2CSFmDlAqwDkEWxsiSnuV3qpSv72h8vLAP'
+message = '唯一支持'
+image_path = 'C:\\Users\\User\\Desktop\\AI\\99.jpg'
 
 with open(image_path, 'rb') as image_file:
     rd = requests.post(
@@ -21,3 +20,10 @@ with open(image_path, 'rb') as image_file:
         data={"message": message},
         files={"imageFile": image_file}
     )
+
+# 檢查請求是否成功
+if rd.status_code != 200:
+    print(f"傳送失敗，錯誤碼：{rd.status_code}")
+    print(f"錯誤訊息：{rd.text}")
+else:
+    print("傳送成功")
